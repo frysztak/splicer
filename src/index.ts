57be8f3680217868b2de2d809770142179ede45e
Sebastian Frysztak <sebastian.frysztak@gmail.com>
2019-09-01 11:10:55 +0200

    add support for drawing points

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ interface IState {
     x: number[];
     y: number[];
     maxY: number;
+    xScale: number,
+    xOffset: number;
+    yScale: number,
+    yOffset: number;
+    points: IPoint[];
+}
+
+interface IPoint {
+    x: number,
+    y: number,
 }
 
 const state: IState = {
@@ -16,14 +26,23 @@ const state: IState = {
     maxY: 1,
     x: [],
     y: [],
+    xScale: 1,
+    xOffset: 0,
+    yScale: 1,
+    yOffset: 0,
+    points: [],
 };
+
+type NumOrArr = number | number[];
 
 const margin = 45;
 const arrowOffset = 8;
+const arrowLength = arrowOffset / Math.sqrt(2);
 const axisCutoff = 25;
 const tickHeight = 8;
 const plotColour = '#bc5090';
 const pointColour = '#ffa600';
+const pointRadius = 8;
 
 function drawAxes() {
     const ctx = state.ctx;
@@ -33,18 +52,18 @@ function drawAxes() {
     ctx.beginPath();
     ctx.moveTo(margin, y);
     ctx.lineTo(finalX, y);
-    ctx.lineTo(finalX - arrowOffset, y-arrowOffset);
+    ctx.lineTo(finalX - arrowOffset, y - arrowOffset);
     ctx.moveTo(finalX, y);
-    ctx.lineTo(finalX - arrowOffset, y+arrowOffset);
+    ctx.lineTo(finalX - arrowOffset, y + arrowOffset);
     ctx.stroke();
     ctx.font = "normal bold 20px sans-serif";
-    ctx.fillText('t', finalX - arrowOffset, y+4*arrowOffset);
+    ctx.fillText('t', finalX - arrowOffset, y + 4 * arrowOffset);
     // tick
     ctx.font = "normal normal 16px sans-serif";
     ctx.textAlign = "center";
-    ctx.fillText('1', finalX - arrowOffset - axisCutoff, y+4*arrowOffset);
+    ctx.fillText('1', finalX - arrowOffset - axisCutoff, y + 4 * arrowOffset);
     ctx.moveTo(finalX - arrowOffset - axisCutoff, y);
-    ctx.lineTo(finalX - arrowOffset - axisCutoff, y+tickHeight);
+    ctx.lineTo(finalX - arrowOffset - axisCutoff, y + tickHeight);
     ctx.stroke();
 
     // y axis
@@ -58,38 +77,89 @@ function drawAxes() {
     ctx.lineTo(margin + arrowOffset, margin + arrowOffset);
     ctx.stroke();
     ctx.font = "normal bold 20px sans-serif";
-    ctx.fillText('y', margin - 4*arrowOffset, margin+arrowOffset);
+    ctx.fillText('y', margin - 4 * arrowOffset, margin + arrowOffset);
     // tick
     ctx.font = "normal normal 16px sans-serif";
     ctx.textBaseline = "middle";
-    ctx.fillText(state.maxY.toFixed(1), margin - 4*arrowOffset, margin + arrowOffset + axisCutoff);
+    ctx.fillText(state.maxY.toFixed(1), margin - 4 * arrowOffset, margin + arrowOffset + axisCutoff);
     ctx.moveTo(margin, margin + arrowOffset + axisCutoff);
     ctx.lineTo(margin - tickHeight, margin + arrowOffset + axisCutoff);
     ctx.stroke();
 }
 
+function updateScale() {
+    const width = state.canvas.width - 2 * margin - axisCutoff - arrowLength;
+    const height = state.canvas.height - 2 * margin - axisCutoff - arrowLength;
+    state.xScale = width / max(state.x);
+    state.xOffset = margin;
+    state.yScale = height / state.maxY;
+    state.yOffset = height + margin + axisCutoff + arrowLength;
+}
+
 function drawPlot() {
     const ctx = state.ctx;
-    const arrowLength = arrowOffset/Math.sqrt(2);
-    const [width, height] = [state.canvas.width - 2*margin - axisCutoff - arrowLength, state.canvas.height - 2*margin - axisCutoff - arrowLength];
-    const [xScale, yScale] = [width / max(state.x), height / state.maxY];
-    const [scaledX, scaledY] = [state.x.map(x => x*xScale + margin), state.y.map(y => height + margin + axisCutoff + arrowLength - y*yScale)];
+    const {x, y} = toScreenSpace(state.x, state.y) as {x: number[], y: number[]};
 
-    ctx.moveTo(scaledX[0], scaledY[0]);
+    ctx.moveTo(x[0], y[0]);
     ctx.beginPath();
     ctx.strokeStyle = plotColour;
     ctx.lineWidth = 2;
-    for (const [x_, y_] of zip(scaledX, scaledY)) {
+    for (const [x_, y_] of zip(x, y)) {
         ctx.lineTo(x_, y_);
     }
     ctx.stroke();
 }
 
+function drawPoints() {
+    const ctx = state.ctx;
+    for (const point of state.points) {
+        ctx.beginPath();
+        const {x, y} = toScreenSpace(point.x, point.y) as IPoint;
+        ctx.arc(x, y, pointRadius, 0, 2 * Math.PI);
+        ctx.fillStyle = pointColour;
+        ctx.fill();
+    }
+}
+
+function toScreenSpace(x: NumOrArr, y: NumOrArr): { x: NumOrArr, y: NumOrArr } {
+    const mapX = (x: number) => state.xOffset + x * state.xScale;
+    const mapY = (y: number) => state.yOffset - y * state.yScale;
+
+    if (Array.isArray(x) && Array.isArray(y)) {
+        return {
+            x: x.map(mapX),
+            y: y.map(mapY),
+        };
+    } else if (typeof x === 'number' && typeof y === 'number')
+        return {
+            x: mapX(x),
+            y: mapY(y),
+        };
+}
+
+function fromScreenSpace(x: NumOrArr, y: NumOrArr): { x: NumOrArr, y: NumOrArr } {
+    const mapX = (x: number) =>  (x - state.xOffset)/state.xScale;
+    const mapY = (y: number) => -(y - state.yOffset)/state.yScale;
+
+    if (Array.isArray(x) && Array.isArray(y)) {
+        return {
+            x: x.map(mapX),
+            y: y.map(mapY),
+        };
+    } else if (typeof x === 'number' && typeof y === 'number')
+        return {
+            x: mapX(x),
+            y: mapY(y),
+        };
+}
+
 function drawFrame() {
     state.canvas.width = state.canvas.offsetWidth * window.devicePixelRatio;
     state.canvas.height = state.canvas.offsetHeight * window.devicePixelRatio;
+    updateScale();
     drawAxes();
     drawPlot();
+    drawPoints();
     requestAnimationFrame(drawFrame);
 }
 
@@ -103,9 +173,15 @@ function start() {
     requestAnimationFrame(drawFrame);
 }
 
+function handleClick(ev: MouseEvent) {
+    const point = fromScreenSpace(ev.clientX - pointRadius, ev.clientY - pointRadius) as IPoint;
+    state.points.push(point);
+}
+
 function hookEventListeners() {
     const maxYInput = <HTMLInputElement>document.getElementById('maxY');
     maxYInput.oninput = ((ev: Event) => state.maxY = Number((ev.target as HTMLInputElement).value));
+    state.canvas.onclick = handleClick;
 }
 
 window.onload = start;
